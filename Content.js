5d265683d135dc452eac10dbc2047f903497e661
Anna <berishviliannaa@gmail.com>
2023-08-22 14:29:35 +0400

    submition

--- Content.js
+++ Content.js
@@ -3,6 +3,9 @@ console.log("chrome extension go!");
 const teckInput = document.getElementById("documentNo");
 const numInput = document.getElementById("vehicleNo2");
 const capInput = document.getElementById("captcha_code");
+const form = document.getElementById("form");
+
+let data = [];
 
 async function fetchFakeData() {
     const jsonUrl = chrome.runtime.getURL("fakejson.json");
@@ -10,10 +13,27 @@ async function fetchFakeData() {
         const response = await fetch(jsonUrl);
         const jsonData = await response.json();
 
-        teckInput.value = jsonData.techPass;
-        numInput.value = jsonData.carNum;
+        data = jsonData.cars; 
+
+        processCars();
     } catch (error) {
         console.error("Error fetching or parsing JSON:", error);
+    }
+}
+
+async function submitFormData(car) {
+
+    teckInput.value = car.techPass;
+    numInput.value = car.carNum;
+
+
+    form.submit();
+}
+
+async function processCars() {
+    for (const car of data) {
+        await submitFormData(car);
+        await new Promise(resolve => setTimeout(resolve, 2000));
     }
 }
 
